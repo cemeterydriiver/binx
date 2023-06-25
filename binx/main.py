fa12ce794dd8f6eceb41215b9b9a9af2d5bf6f9a
ashlyn <102593108+cemeterydriiver@users.noreply.github.com>
2023-06-24 23:35:22 -0300

    Update main.py

--- binx/main.py
+++ binx/main.py
@@ -1,14 +1,14 @@
-#importando bibliotecas (a do audio já vem quando é baixada)
-
 import speech_recognition as sr
 import pyttsx3
 import datetime
 import wikipedia
 import pywhatkit
+import webbrowser
 
-#objeto para reconhecer o audio
-
+# Inicializando o objeto para reconhecer o áudio
 audio = sr.Recognizer()
+
+# Inicializando o objeto para sintetizar a fala
 binx = pyttsx3.init()
 binx.setProperty('volume', 0.8)
 binx.setProperty('rate', 150)
@@ -18,7 +18,7 @@ binx.runAndWait()
 
 
 def executaComando():
-    comando = "" 
+    comando = ""
     try:
         with sr.Microphone() as source:
             print('Ouvindo...')
@@ -38,7 +38,7 @@ def comandoVozUser():
     comando = executaComando()
     if 'horas' in comando:
         hora = datetime.datetime.now().strftime('%H:%M')
-        binx.say('Agora são' +hora)
+        binx.say('Agora são ' + hora)
         binx.runAndWait()
     elif 'procure por' in comando:
         procurar = comando.replace('procure por', '')
@@ -49,7 +49,51 @@ def comandoVozUser():
         binx.runAndWait()
     elif 'toque' in comando:
         musica = comando.replace('toque', '')
-        result = pywhatkit.playonyt(musica)
+        pywhatkit.playonyt(musica)
         binx.say('Tocando música')
         binx.runAndWait()
+    elif 'criar uma tarefa' in comando:
+        tarefa = comando.replace('criar uma tarefa', '').strip()
+        if tarefa:
+            with open('tarefas.txt', 'a') as arquivo:
+                arquivo.write(f'{tarefa}\n')
+            binx.say('Tarefa adicionada à lista de tarefas.')
+            binx.runAndWait()
+        else:
+            binx.say('Por favor, especifique a tarefa.')
+            binx.runAndWait()
+    elif 'ler minha lista de tarefas' in comando:
+        with open('tarefas.txt', 'r') as arquivo:
+            lista_tarefas = arquivo.read()
+        if lista_tarefas:
+            binx.say('Aqui está a sua lista de tarefas:')
+            binx.say(lista_tarefas)
+            binx.runAndWait()
+        else:
+            binx.say('Sua lista de tarefas está vazia.')
+            binx.runAndWait()
+    elif 'definir um lembrete' in comando:
+        lembrete = comando.replace('definir um lembrete', '').strip()
+        if lembrete:
+            agora = datetime.datetime.now()
+            hora_atual = agora.strftime('%H:%M')
+            with open('tarefas.txt', 'a') as arquivo:
+                arquivo.write(f'{lembrete} às {hora_atual}\n')
+            binx.say('Lembrete definido.')
+            binx.runAndWait()
+        else:
+            binx.say('Por favor, especifique o lembrete.')
+            binx.runAndWait()
+    elif 'pesquisar na web por' in comando:
+        consulta = comando.replace('pesquisar na web por', '').strip()
+        if consulta:
+            url = f'https://www.google.com/search?q={consulta}'
+            webbrowser.open(url)
+            binx.say('Aqui estão os resultados da pesquisa.')
+            binx.runAndWait()
+        else:
+            binx.say('Por favor, especifique a consulta de pesquisa.')
+            binx.runAndWait()
+
+
 comandoVozUser()
