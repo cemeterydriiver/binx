b618cbec9ed019cba741c96efbf5cc03b8293f08
ashlyn <102593108+cemeterydriiver@users.noreply.github.com>
2023-09-11 21:56:06 -0300

    Update main.py

--- binx/main.py
+++ binx/main.py
@@ -3,94 +3,122 @@ import pyttsx3
 import datetime
 import pywhatkit
 import webbrowser
-import subprocess
 import os
 import time
 
-audio = sr.Recognizer()
-binx = pyttsx3.init()
-binx.setProperty('volume', 0.8)
-binx.setProperty('rate', 150)
-binx.say('Olá, eu sou o Binx')
-binx.say('Como posso te ajudar?')
-binx.runAndWait()
+def binxInit():
+    binx = pyttsx3.init()
+    binx.setProperty('volume', 0.8)
+    binx.setProperty('rate', 150)
+    binx.say('Olá, eu sou o Binx')
+    binx.say('Como posso te ajudar?')
+    binx.runAndWait()
+    return binx
 
-sitesEducacionais = [['geekieOne', 'https://one.geekie.com.br/'], 
-                     ['classroom', 'https://classroom.google.com/u/0/h'], 
-                     ['espaço do estudante', 'https://estudante.sesisenai.org.br/login']]
+def getSchoolSites():
+    return [['geekieOne', 'https://one.geekie.com.br/'],
+            ['classroom', 'https://classroom.google.com/u/0/h'],
+            ['espaço do estudante', 'https://estudante.sesisenai.org.br/login']]
 
-def executaComando():
-    comando = ""
+def executeComand(binx, audio):
+    audio = sr.Recognizer()
     try:
         with sr.Microphone() as source:
             print('Ouvindo...')
-            voz = audio.listen(source)
-            comando = audio.recognize_google(voz, language='pt-BR')
-            comando = comando.lower()
-            if 'binx' in comando:
-                comando = comando.replace('binx', '')
-                binx.say(comando)
-                binx.runAndWait()
-    except:
-        print('ERRO: Microfone não está funcionando.')
-    return comando
+            voice = audio.listen(source)
+            command = audio.recognize_google(voice, language='pt-BR').lower()
+            
+            if 'binx' in command:
+                command = command.replace('binx', '')
+                binx.say(command)
+                binx.runAndWait()            
+            return command
+        
+    except sr.UnknownValueError:
+        print('ERRO: Não foi possível entender o áudio.')
+        return ""
+    except sr.RequestError:
+        print('ERRO: Não foi possível acessar o serviço de reconhecimento de fala.')
+        return ""
+    
+def hours(binx):
+    hora = datetime.datetime.now().strftime('%H:%M')
+    binx.say('Agora são ' + hora)
+    binx.runAndWait()
 
-def comandoVozUser():
+def searchWeb(command, binx):
+    query = command.replace('pesquisar na web por', '').strip()
+    if query:
+        url = f'https://www.google.com/search?q={query}'
+        webbrowser.open(url)
+        binx.say('Aqui estão os resultados da pesquisa.')
+        binx.runAndWait()
+    else:
+        binx.say('Por favor, especifique a consulta de pesquisa.')
+        binx.runAndWait()
+
+def openSchoolSites(command, schoolSites, binx):
+    query = command.replace('abrir', '').strip()
+    findedSite = False
+    for site in schoolSites:
+        if site[0].lower() in query:
+            webbrowser.open(site[1])
+            binx.say('Bons estudos!')
+            binx.runAndWait()
+            findedSite = True
+            break
+    if not findedSite:
+        binx.say('Site educacional não encontrado na lista.')
+        binx.runAndWait()
+
+def playMusic(command, binx):
+    music = command.replace('toque', '')
+    pywhatkit.playonyt(music)
+    binx.say('Tocando música')
+    binx.runAndWait()
+
+def shutdownComputer(command, binx):
+    if 'uma hora' in command:
+        os.system("shutdown -s -t 3600")
+    elif 'meia hora' in command:
+        os.system("shutdown -s -t 1800")
+    elif 'agora' in command:
+        for i in range(5, 0, -1):
+            binx.say(f'Desligando o pc em {i}...')
+            time.sleep(1)
+        os.system("shutdown -s -t 0")
+
+def cancelShutdown(binx):
+    os.system("shutdown -a")
+
+def main():
+    audio = sr.Recognizer()
+    binx = binxInit()
+    schoolSites = getSchoolSites()
+
     while True:
-        comando = executaComando()
-
-        if 'sair' in comando:
+        command = executeComand(audio, binx)
+        
+        if 'sair' in command:
             binx.say('Até mais!')
             binx.runAndWait()
             break
+        if 'horas' in command:
+            hours(binx)
 
-        if 'horas' in comando:
-            hora = datetime.datetime.now().strftime('%H:%M')
-            binx.say('Agora são ' + hora)
-            binx.runAndWait()
+        elif 'pesquisar na web por' in command:
+            searchWeb(command, binx)
 
-        elif 'pesquisar na web por' in comando:
-            consulta = comando.replace('pesquisar na web por', '').strip()
-            if consulta:
-                url = f'https://www.google.com/search?q={consulta}'
-                webbrowser.open(url)
-                binx.say('Aqui estão os resultados da pesquisa.')
-                binx.runAndWait()
-            else:
-                binx.say('Por favor, especifique a consulta de pesquisa.')
-                binx.runAndWait()
+        elif 'abrir' in command:
+            openSchoolSites(command, schoolSites, binx)
 
-        elif 'abrir' in comando:
-            consulta = comando.replace('abrir', '').strip()
-            siteEncontrado = False
-            for site in sitesEducacionais:
-                if site[0].lower() in consulta:
-                    webbrowser.open(site[1])
-                    binx.say('Bons estudos!')
-                    binx.runAndWait()
-                    siteEncontrado = True
-                    break
-            if not siteEncontrado:
-                binx.say('Site educacional não encontrado na lista.')
-                binx.runAndWait()
-
-        elif 'toque' in comando:
-            musica = comando.replace('toque', '')
-            pywhatkit.playonyt(musica)
-            binx.say('Tocando música')
-            binx.runAndWait()
-
-        elif 'desligar computador' in comando and 'uma hora' in comando:
-            os.system("shutdown -s -t 3600")
-        elif 'desligar computador' in comando and 'meia hora' in comando:
-            os.system("shutdown -s -t 1800")
-        elif 'desligar computador' in comando and 'agora' in comando:
-            for i in range(5, 0, -1):
-                binx.say(f'Desligando o pc em {i}...')
-                time.sleep(1)  
-            os.system("shutdown -s -t 0")
-        elif 'cancelar desligamento' in comando:
-            os.system("shutdown -a")
+        elif 'toque' in command:
+            playMusic(command, binx)
+        
+        elif 'desligar computador' in command:
+            shutdownComputer(command, binx)
+        elif 'cancelar desligamento' in command:
+            cancelShutdown(binx)
 
 if __name__ == "__main__":
-    comandoVozUser()
+    main()
